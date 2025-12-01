929944882c3d17d4b3f190de203ef9b6d9303037
unknown <kd1366728@kobedenshi078.onmicrosoft.com>
2025-12-01 11:34:26 +0900

    kkk

--- app/src/main/java/com/example/team1application/MainActivity.kt
+++ app/src/main/java/com/example/team1application/MainActivity.kt
@@ -13,10 +13,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             Team1ApplicationTheme{
                     RirekiScreen()
-                }
             }
         }
     }
+}
 //class MainActivity : ComponentActivity() {
 //    override fun onCreate(savedInstanceState: Bundle?) {
 //        super.onCreate(savedInstanceState)
