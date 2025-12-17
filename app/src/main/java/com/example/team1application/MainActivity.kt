41e84d42af65930053a985aac1bba70ec8edb5b3
Y4hoo <kd1366739@st.kobedenshi.ac.jp>
2025-12-17 14:41:26 +0900

    mainの警告を減らした

--- app/src/main/java/com/example/team1application/MainActivity.kt
+++ app/src/main/java/com/example/team1application/MainActivity.kt
@@ -1,47 +1,27 @@
 package com.example.team1application
 
-import android.provider.Settings
 import android.app.AlarmManager
 import android.content.Intent
-import android.media.audiofx.EnvironmentalReverb
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
+import android.provider.Settings
 import androidx.activity.ComponentActivity
-import androidx.compose.ui.graphics.Color
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-
 import com.example.team1application.ui.theme.Team1ApplicationTheme
-import androidx.compose.animation.Crossfade
-import androidx.compose.animation.core.tween
-import java.time.Clock
-
 
 
 class MainActivity : ComponentActivity() {
     private lateinit var alarmInitializer: AlarmInitializer
-
-    //  今後このoncleate多分消えてなくなるからどっかに避難
-    @RequiresApi(Build.VERSION_CODES.S)
-    private var currentTextSize: Float = 16f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
